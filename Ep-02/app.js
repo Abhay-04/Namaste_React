c836c40846260a0a530875d8af17ff2a2bf29609
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2024-08-27 21:05:12 +0530

    npm react

---
+++ Ep-02/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", {id: "Heading_1"}, "Heading1"),
+      React.createElement("h2", {id: "Heading_2"}, "Heading2"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {id: "Heading_1"}, "Heading1"),
+      React.createElement("h2", {id: "Heading_2"}, "Heading2"),
+    ]),
+  ]);
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  root.render(parent);
